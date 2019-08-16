65c1a713ba146920a5ec02a694825c83861d58b7
Dona <dona.john@in.theory-y.com>
2019-08-16 16:18:59 +0530

    question 1

---
+++ question1.py
@@ -0,0 +1,8 @@
+def NumFunc(myList1=[],myList2=[],*args):
+    list3  = list(set(myList1).intersection(myList2))
+    return list3
+
+
+myList1 = [1,2,3,4,5,6]
+myList2 = [3, 5, 7, 9]
+NumFunc(myList1,myList2)
